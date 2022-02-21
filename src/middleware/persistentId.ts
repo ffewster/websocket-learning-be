b799f4fc343ae32c29edb735dfb4cf41fdf8a009
ffewster <ffewster@twigeducation.com>
2022-02-21 10:45:50 +0000

    Lint

--- src/middleware/persistentId.ts
+++ src/middleware/persistentId.ts
@@ -1,51 +1,51 @@
 import { Socket } from "socket.io";
 import { ExtendedError } from "socket.io/dist/namespace";
 import {
-  ClientToServerEvents,
-  ServerToClientEvents,
-  InterServerEvents,
-  SocketData,
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData,
 } from "src/types";
 import { randomId, sessionStore } from "../utils";
 
 export const persistentIdMiddleware = (
-  socket: Socket<
+    socket: Socket<
     ClientToServerEvents,
     ServerToClientEvents,
     InterServerEvents,
     SocketData
   >,
-  next: (err?: ExtendedError | undefined) => void
+    next: (err?: ExtendedError | undefined) => void
 ) => {
-  const sessionID = socket.handshake.auth.sessionID;
-  if (sessionID) {
+    const sessionID = socket.handshake.auth.sessionID;
+    if (sessionID) {
     // find existing session
-    const session = sessionStore.findSession(sessionID);
-    if (session) {
-      socket.data.sessionID = sessionID;
-      socket.data.userID = session.userID;
-      socket.data.username = session.username;
-      sessionStore.saveSession(sessionID, {
-        ...session,
-        connected: true,
-      });
-      return next();
+        const session = sessionStore.findSession(sessionID);
+        if (session) {
+            socket.data.sessionID = sessionID;
+            socket.data.userID = session.userID;
+            socket.data.username = session.username;
+            sessionStore.saveSession(sessionID, {
+                ...session,
+                connected: true,
+            });
+            return next();
+        }
     }
-  }
-  const username = socket.handshake.auth.username;
-  if (!username) {
-    return next(new Error("invalid username"));
-  }
-  // create new session
-  socket.data.sessionID = randomId();
-  socket.data.userID = randomId();
-  socket.data.username = username;
-  const { userID, sessionID: newSessionID } = socket.data;
-  sessionStore.saveSession(newSessionID, {
-    connected: true,
-    userID,
-    username,
-  });
-  sessionStore.debug();
-  next();
+    const username = socket.handshake.auth.username;
+    if (!username) {
+        return next(new Error("invalid username"));
+    }
+    // create new session
+    socket.data.sessionID = randomId();
+    socket.data.userID = randomId();
+    socket.data.username = username;
+    const { userID, sessionID: newSessionID } = socket.data;
+    sessionStore.saveSession(newSessionID, {
+        connected: true,
+        userID,
+        username,
+    });
+    sessionStore.debug();
+    next();
 };
